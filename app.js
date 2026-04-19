ad12bd4f9a1b888f30557e4f598d1aa6cdde0ec5
Tharun-GK <tharungk202@gmail.com>
2026-04-19 14:05:33 +0530

    cloud mini project

--- app.js
+++ app.js
@@ -1,37 +1,72 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhw2uuMpZ1UmB1itFFwk8V3RIvXhOeQVA",
+  authDomain: "cloud-chat-app-493311.firebaseapp.com",
+  projectId: "cloud-chat-app-493311",
+  storageBucket: "cloud-chat-app-493311.firebasestorage.app",
+  messagingSenderId: "660856358009",
+  appId: "1:660856358009:web:1b2fe5606bd328543800a8",
+  measurementId: "G-T9GRFCLYZP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
 let currentUser = "";
 
-// Fake signup
+// Signup
 function signup() {
-    alert("Signup (Firebase not connected yet)");
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    auth.createUserWithEmailAndPassword(email, password)
+        .then(() => alert("Signup successful"))
+        .catch(err => alert(err.message));
 }
 
-// Fake login
+// Login
 function login() {
     const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-    if (!email) {
-        alert("Enter email");
-        return;
-    }
-
-    currentUser = email;
-
-    document.getElementById("auth").style.display = "none";
-    document.getElementById("chat").style.display = "block";
+    auth.signInWithEmailAndPassword(email, password)
+        .then(() => {
+            currentUser = email;
+            document.getElementById("auth").style.display = "none";
+            document.getElementById("chat").style.display = "block";
+            loadMessages();
+        })
+        .catch(err => alert(err.message));
 }
 
 // Send message
 function sendMessage() {
     const msg = document.getElementById("messageInput").value;
 
-    if (!msg) return;
-
-    const time = new Date().toLocaleTimeString();
-
-    const msgDiv = document.createElement("div");
-    msgDiv.textContent = `[${time}] ${currentUser}: ${msg}`;
-
-    document.getElementById("messages").appendChild(msgDiv);
+    db.collection("messages").add({
+        text: msg,
+        user: currentUser,
+        timestamp: new Date()
+    });
 
     document.getElementById("messageInput").value = "";
+}
+
+// Load messages (real-time)
+function loadMessages() {
+    db.collection("messages")
+      .orderBy("timestamp")
+      .onSnapshot(snapshot => {
+        const messagesDiv = document.getElementById("messages");
+        messagesDiv.innerHTML = "";
+
+        snapshot.forEach(doc => {
+            const data = doc.data();
+            const msgDiv = document.createElement("div");
+            msgDiv.textContent = `${data.user}: ${data.text}`;
+            messagesDiv.appendChild(msgDiv);
+        });
+    });
 }
